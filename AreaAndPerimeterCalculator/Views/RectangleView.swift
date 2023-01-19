02423ca9a10cb131bb15567b0353faa178c835c9
Isaad Khan <IKhan@lcs.on.ca>
2023-01-19 11:34:37 -0500

    Added properties to the structure to begin making the static interface to become interactive

--- AreaAndPerimeterCalculator/Views/RectangleView.swift
+++ AreaAndPerimeterCalculator/Views/RectangleView.swift
@@ -8,6 +8,18 @@
 import SwiftUI
 
 struct RectangleView: View {
+    
+    // MARK: Stored properties
+    let length: Double = 100
+    let width: Double = 5
+    
+    // MARK: Computed properties
+    var area: Double {
+        return length * width
+    }
+    
+    
+    // Expressing the user interface
     var body: some View {
         VStack(spacing: 15) {
             
@@ -24,17 +36,21 @@ struct RectangleView: View {
                 .font(.title2)
                 .bold()
             
-            Text("7.0")
+            // Udr string interpolation to convert
+            // the double data type to text (string)
+            //
+            // \(variable)
+            Text("\(length)")
                 .font(.title2)
            
             Text("Width")
                 .font(.title2)
                 .bold()
             
-            Text("5.0")
+            Text("\(width)")
                 .font(.title2)
             
-            Text("Area")
+            Text("\(area)")
                 .font(.title2)
                 .bold()
             
